4cfc346dc5d3704cb5464d9631c0ca0ca865a80f
Franco Rutigliano <francorutigliano11@gmail.com>
2023-08-07 19:11:34 -0300

    [FEATURES] Template navbar agregado /CDN bootstrap

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,1 +1,23 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+from django import forms
+
+from django.forms.widgets import PasswordInput, TextInput
+
+# Registros de usuarios y creacion de usuarios
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+
+# Logeo de usuarios
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
